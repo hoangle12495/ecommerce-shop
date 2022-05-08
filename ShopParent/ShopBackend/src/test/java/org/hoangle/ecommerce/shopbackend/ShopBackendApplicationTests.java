1313401d2feb2d16e52edd409749bee92bfbda8d
hoangvle <hoangvle@kms-technology.com>
2022-05-08 11:00:23 +0700

    Project Initiate

---
+++ ShopParent/ShopBackend/src/test/java/org/hoangle/ecommerce/shopbackend/ShopBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package org.hoangle.ecommerce.shopbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ShopBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
